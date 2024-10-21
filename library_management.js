da8d6b7f9b0befe97d2a924b59582e209040858c
Bilal Patel <bilalpatel@outlook.com>
2024-10-20 21:36:29 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -1,4 +1,4 @@
-
+// Task 1 
 // Define class of book
 
 class Book {
@@ -40,7 +40,11 @@ if(typeof status === 'boolean') {
 
 // remove // from ^^^^^^^^^
 
-//defining the class section
+// TASK 2
+
+//defining the class section 
+
+
 
 class Section {
     constructor(name) {
@@ -70,14 +74,73 @@ return this.books.filter(book => book.isAvailable).length;
 }
 
 // Test Below
-const book1 = new Book  ("the game", "Neal Yates", "123098");
-const book2 = new Book("The new Game", "Kyle Malone", "123097");
+// const book1 = new Book  ("the game", "Neal Yates", "123098");
+// const book2 = new Book("The new Game", "Kyle Malone", "123097");
 
-// Create a new section
-const fiction = new Section("Fiction");
+// // Create a new section
+// const fiction = new Section("Fiction");
 
-//Add books to the section 
-fiction.addBook(book1).addBook(book2);
+// //Add books to the section 
+// fiction.addBook(book1).addBook(book2);
 
-// List books in the section to see their details and availability
-fiction.listBooks();
+// // List books in the section to see their details and availability
+// fiction.listBooks();
+
+// // define the class
+
+// TASK 3 
+
+
+class Patron {
+    constructor(name) {
+        this.name = name;
+        this.borrowedBooks = [];
+    }
+
+//borrow book is available
+
+borrowedBook (book) {
+    if (book.isAvailable) { // check is book is available
+        book.isAvailable = false; // update status
+        this.borrowedBooks.push(book);
+        console.log(`${book.title} is borrowed by ${this.name}`)
+    } else{
+        console.log(`Not available: ${book.title}`)
+    }
+}
+// return borrowed book
+ returnBook (book) {
+ const index = this.borrowedBooks.indexOf(book); // find using index method
+
+
+ //check if book is available
+ if (index !== -1) {book.isAvailable = true;
+    this.borrowedBooks.splice(index, 1); //remove book from the borrow array
+    console.log(`${book.title} returned by ${this.name}`); /// log return
+ } else {
+
+    console.log(`${book.title} not borrowed by ${this.name}`);
+ }
+
+}
+}
+
+// // Test Below by removing //
+// const book1 = new Book  ("the game", "Neal Yates", "123098");
+
+
+// // Create a new patron
+// const patron = new Patron("Steve");
+
+// //Add books to the patron borrows
+// patron.borrowedBooks.push(book1);
+// book1.isAvailable = false;
+
+// // reutrn book
+// patron.returnBook(book1);
+
+
+/// remove // above^^^^^^^^^^^^^^^^^^
+
+// ouput: the game returned by Steve 
+
