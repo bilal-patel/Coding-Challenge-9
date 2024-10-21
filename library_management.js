f3912c52dfe057967ea392ec1813e9463646d6d8
Bilal Patel <bilalpatel@outlook.com>
2024-10-20 20:49:58 -0400

    Create Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -40,3 +40,44 @@ if(typeof status === 'boolean') {
 
 // remove // from ^^^^^^^^^
 
+//defining the class section
+
+class Section {
+    constructor(name) {
+        this.name = name;
+        this.books = []
+    }
+
+    // add a book object to the books array
+    addBook(book) {
+        this.books.push(book);
+        return this;
+    }
+
+    // return total avialable books
+    getAvailablebooks() {
+return this.books.filter(book => book.isAvailable).length;
+    }
+
+    // listing all the books in the section
+    listBooks () {
+        console.log (`Books in the ${this.name} section`);
+        this.books.forEach(book =>
+            console.log(`- ${book.title} (${book.isAvailable ? "Available" : "Borrowed"})`)
+        );
+    }
+
+}
+
+// Test Below
+const book1 = new Book  ("the game", "Neal Yates", "123098");
+const book2 = new Book("The new Game", "Kyle Malone", "123097");
+
+// Create a new section
+const fiction = new Section("Fiction");
+
+//Add books to the section 
+fiction.addBook(book1).addBook(book2);
+
+// List books in the section to see their details and availability
+fiction.listBooks();
